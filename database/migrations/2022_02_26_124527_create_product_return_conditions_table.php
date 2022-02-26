628bffe26394284999de6d0df5d711310b358c42
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2022-02-26 18:18:30 +0530

    Create 2022_02_26_124527_create_product_return_conditions_table.php

---
+++ database/migrations/2022_02_26_124527_create_product_return_conditions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductReturnConditionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_return_conditions', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('title');
+            $table->integer('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_return_conditions');
+    }
+}
