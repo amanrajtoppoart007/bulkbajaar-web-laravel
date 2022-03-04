50de93821c84ca2417b26b5333183122972dbb46
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2022-03-04 14:51:36 +0530

    Update ChangePasswordRequest.php

--- app/Http/Requests/Api/ChangePasswordRequest.php
+++ app/Http/Requests/Api/ChangePasswordRequest.php
@@ -6,6 +6,9 @@ use App\Rules\CheckIfSameAsOldPassword;
 use App\Rules\MatchCurrentPassword;
 use Illuminate\Foundation\Http\FormRequest;
 
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
 class ChangePasswordRequest extends FormRequest
 {
     /**
@@ -30,5 +33,16 @@ class ChangePasswordRequest extends FormRequest
             'password'=>['required',new CheckIfSameAsOldPassword],
             'password_confirmation'=>['required_with:password|same:password']
         ];
+    }
+
+     protected function failedValidation(Validator $validator)
+    {
+        $msg='';
+        foreach($validator->errors()->all() as $error)
+        {
+            $msg .= $error."\n";
+        }
+        $result = ["status"=>0,"response"=>"validation_error","message"=>$msg];
+        throw new HttpResponseException(response()->json($result, 200));
     }
 }
